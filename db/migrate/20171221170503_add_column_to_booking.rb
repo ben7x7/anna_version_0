ec46b37e895f79673babc54adc410234263ff621
benoit bessiere <benoit.bessiere@gmail.com>
2017-12-21 19:13:00 +0100

    generated model Booking

---
+++ db/migrate/20171221170503_add_column_to_booking.rb
@@ -0,0 +1,7 @@
+class AddColumnToBooking < ActiveRecord::Migration[5.1]
+  def change
+    add_column :bookings, :date, :date
+    add_column :bookings, :starting_time, :date
+    add_column :bookings, :ending_time, :date
+  end
+end
