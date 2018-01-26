8f7db086010326127a9e8cb816aeab04d992bece
benoit bessiere <benoit.bessiere@gmail.com>
2018-01-26 14:09:49 +0100

    background sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,15 @@ class User < ApplicationRecord
 
   has_attachment :photo
 
+
+
+  USER_TYPES = ["costumer", "employee", "manager"]
+  validates :user_type, inclusion: {in: USER_TYPES}
+
   validates :first_name, presence: true
   validates :last_name, presence: true
+
+  CAPACITIES = ["Full", "Level 1", "Level 2", "Level 3", "Under tutel"]
+  validates :capacity, inclusion: {in: CAPACITIES}
 
 end
