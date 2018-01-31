7a6d35b76a08e436c72ca51b038401fc54d8aca7
benoit bessiere <benoit.bessiere@gmail.com>
2018-01-31 21:45:14 +0100

    more seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
 Booking.destroy_all
+Employee.destroy_all
 Service.destroy_all
 Agency.destroy_all
 User.destroy_all
@@ -34,13 +35,13 @@ agency2 = Agency.create!(
 agency3 = Agency.create!(
   brand: 'Anna Call Center',
   created_date: '2018',
-  owner: 'M. Anna',
-  director: 'M. Anna',
+  owner: 'Mr. Hanna',
+  director: 'Mrs Hanna',
   address: '14 Bvd Haussmann 75009 Paris',
   phone: '0144332211',
-  contact_person: 'Paul Anna',
+  contact_person: 'Anna Hanna',
   mobile: '0788990011',
-  email: 'anna@anna.com',
+  email: 'anna@anna.fr',
   description: 'Call center is available from 8:00 to 18:00 from Monday to Friday to answer to your question and assist you to book a new service. '
 )
 
@@ -70,14 +71,14 @@ service3 = Service.create!(
 
 service4 = Service.create!(
   activity: 'Conciergerie',
-  hourly_fee: '',
-  description: 'Ticket corner for concert, operas, painting exposition...',
+  hourly_fee: '100',
+  description: 'Any Services on demand...',
   agency: agency2,
   picto: File.open("#{Rails.root.to_s}/app/assets/images/conciergerie.png")
 )
 
 
-user = User.create!(
+user1 = User.create!(
   email: 'james@007.com',
   password: 'password',
   user_type: 'client',
@@ -100,15 +101,169 @@ user = User.create!(
 )
 
 user2 = User.create!(
-  email: 'maria@lapanaosse.fr',
+  email: 'omer@dalors.com',
+  password: 'password',
+  user_type: 'client',
+  first_name: 'Omer',
+  last_name: 'Dalors',
+  phone: '0188575554',
+  mobile: '0609881726',
+  st_number: '55',
+  street: 'Avenue du Chateau',
+  zip: '94100',
+  city: 'Vincennes',
+  building: 'Résidence du Parc',
+  code: '3472A',
+  floor: '6',
+  lift: true,
+  door: 'blue door',
+  comment: 'Please be very carefull with the dishes !',
+  capacity: 'Full',
+  photo_url: 'http://res.cloudinary.com/bessiere/image/upload/v1504284222/omer.jpg'
+)
+
+user3 = User.create!(
+  email: 'pierre@lapanosse.fr',
+  password: 'password',
+  user_type: 'manager',
+  first_name: 'Pierre',
+  last_name: 'Kimousse',
+  phone: '0138273646',
+  mobile: '0611223344',
+  st_number: '28',
+  street: 'Rue de la Solidarité',
+  zip: '75019',
+  city: 'Paris',
+  building: '',
+  code: '',
+  floor: '',
+  lift: true,
+  door: '',
+  comment: '',
+  capacity: 'Full',
+  photo_url: 'http://res.cloudinary.com/bessiere/image/upload/v1513867474/al4ulrsd1vweknj4omjq.jpg'
+)
+
+user4 = User.create!(
+  email: 'aude@lapanosse.fr',
   password: 'password',
   user_type: 'employee',
-  first_name: 'Maria',
-  last_name: 'Da Silva',
-  phone: '0155555555',
+  first_name: 'Aude',
+  last_name: 'Javel',
+  phone: '0154015400',
+  mobile: '0619283746',
+  st_number: '3',
+  street: 'Rue Chapon',
+  zip: '93300',
+  city: 'Aubervilliers',
+  building: '',
+  code: '',
+  floor: '',
+  lift: true,
+  door: '',
+  comment: '',
+  capacity: 'Full',
+  photo_url: 'http://res.cloudinary.com/bessiere/image/upload/v1515865523/mariadasilva.png'
+)
+
+user5 = User.create!(
+  email: 'eva@lapanosse.fr',
+  password: 'password',
+  user_type: 'employee',
+  first_name: 'Eva',
+  last_name: 'Luation',
+  phone: '0155578854',
+  mobile: '0727387496',
+  st_number: '58',
+  street: 'Rue du Midi',
+  zip: '93100',
+  city: 'Montreuil',
+  building: '',
+  code: '',
+  floor: '',
+  lift: true,
+  door: '',
+  comment: '',
+  capacity: 'Full',
+  photo_url: 'http://res.cloudinary.com/bessiere/image/upload/v1503060190/silvia_u1cokf.jpg'
+)
+
+user6 = User.create!(
+  email: 'jessica@lapanosse.fr',
+  password: 'password',
+  user_type: 'employee',
+  first_name: 'Jessica',
+  last_name: 'Lité',
+  phone: '0194939291',
   mobile: '0619283746',
   st_number: '14',
-  street: 'av Foch',
+  street: 'Rue Descartes',
+  zip: '93170',
+  city: 'Bagnolet',
+  building: '',
+  code: '',
+  floor: '',
+  lift: true,
+  door: '',
+  comment: '',
+  capacity: 'Full',
+  photo_url: 'http://res.cloudinary.com/bessiere/image/upload/v1503060190/laure_eklesg.jpg'
+)
+
+user7 = User.create!(
+  email: 'claire@lapanosse.fr',
+  password: 'password',
+  user_type: 'employee',
+  first_name: 'Claire',
+  last_name: 'Gebois',
+  phone: '0154555788',
+  mobile: '0667686768',
+  st_number: '3',
+  street: 'Rue Lamartine',
+  zip: '93130',
+  city: 'Noisy-le-Sec',
+  building: '',
+  code: '',
+  floor: '',
+  lift: true,
+  door: '',
+  comment: '',
+  capacity: 'Full',
+  photo_url: 'http://res.cloudinary.com/bessiere/image/upload/v1503060189/claire_chue5f.jpg'
+)
+
+user8 = User.create!(
+  email: 'patricia@golden-keys-services.fr',
+  password: 'password',
+  user_type: 'manager',
+  first_name: 'Patricia',
+  last_name: 'Tatave',
+  phone: '0144839400',
+  mobile: '0642690139',
+  st_number: '97',
+  street: 'Rue Caulaincour',
+  zip: '75018',
+  city: 'Paris',
+  building: '',
+  code: 'interphone',
+  floor: '3',
+  lift: true,
+  door: 'where there is welcome carpet',
+  comment: 'Pick up keys at the concierge desk',
+  capacity: 'Full',
+  photo_url: 'http://res.cloudinary.com/bessiere/image/upload/v1502989409/patricia.jpg'
+)
+
+user9 = User.create!(
+  email: 'Sarah@golden-keys-services.fr',
+  password: 'password',
+  user_type: 'employee',
+  first_name: 'Sarah',
+  last_name: 'Larousse',
+  phone: '0128719455',
+  mobile: '0621773433',
+  st_number: '103',
+  street: 'Rue de la Paix',
   zip: '75016',
   city: 'Paris',
   building: '',
@@ -117,26 +272,96 @@ user2 = User.create!(
   lift: true,
   door: '',
   comment: '',
-  capacity: '',
-  photo_url: 'http://res.cloudinary.com/bessiere/image/upload/v1515865523/mariadasilva.png'
+  capacity: 'Full',
+  photo_url: 'http://res.cloudinary.com/bessiere/image/upload/v1503060190/sarah_oj4jw0.jpg'
+)
+
+employee1 = Employee.create!(
+  activity:'Housekeper',
+  contract: 'CDI',
+  hired_date: '01/07/2010',
+  rib: 'FR1212345123451234567A01234',
+  hourly_rate: '20',
+  agency: agency1,
+  user: user4,
+)
+
+employee2 = Employee.create!(
+  activity:'Housekeper',
+  contract: 'CDD',
+  hired_date: '01/10/2012',
+  rib: 'FR1212345123451234567B01234',
+  hourly_rate: '22',
+  agency: agency1,
+  user: user5,
+)
+
+employee3 = Employee.create!(
+  activity:'Ironer',
+  contract: 'CDI',
+  hired_date: '01/07/2015',
+  rib: 'FR1212345123451234567C01234',
+  hourly_rate: '20',
+  agency: agency1,
+  user: user6,
+)
+
+employee4 = Employee.create!(
+  activity:'Ironer',
+  contract: 'CDD',
+  hired_date: '01/08/2014',
+  rib: 'FR1212345123451234567D01234',
+  hourly_rate: '18',
+  agency: agency1,
+  user: user7,
+)
+
+employee5 = Employee.create!(
+  activity:'Manager',
+  contract: 'CDI',
+  hired_date: '01/05/2010',
+  rib: 'FR1212345123451234567E01234',
+  hourly_rate: '50',
+  agency: agency1,
+  user: user3,
+)
+
+employee6 = Employee.create!(
+  activity:'Concierge',
+  contract: 'CDI',
+  hired_date: '15/01/2018',
+  rib: 'FR1212345123451234567F01234',
+  hourly_rate: '80',
+  agency: agency2,
+  user: user8,
+)
+
+employee7 = Employee.create!(
+  activity:'Concierge',
+  contract: 'CDI',
+  hired_date: '15/01/2018',
+  rib: 'FR1212345123451234567F01234',
+  hourly_rate: '75',
+  agency: agency2,
+  user: user9,
 )
 
 Booking.create!(
-  status: 'reserved',
+  status: 'reservation',
   date: '15/03/2018',
   starting_time: '08:00',
   ending_time: '10:00',
-  user: user,
+  user: user1,
   service: service1,
   price: '45',
 )
 
 booking_2 = Booking.new(
-  status: 'reserved',
+  status: 'reservation',
   date: '22/03/2018',
   starting_time: '08:00',
   ending_time: '10:00',
-  user: user,
+  user: user2,
   service: service1,
   price: '45',
 )
