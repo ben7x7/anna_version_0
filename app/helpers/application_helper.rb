f5705258467b180c4fb3e30ed5f6209b9b31a950
benoit bessiere <benoit.bessiere@gmail.com>
2018-02-06 11:42:50 +0100

    add favicon

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def image_path_for(user)
+    if user.photo.present?
+      cl_image_path user.photo.path
+    else
+      image_path "avatar-inside-o-circle.png"
+    end
+  end
 end
